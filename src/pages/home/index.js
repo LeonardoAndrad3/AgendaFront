7d60141cf7abd1f2701050797b34e7d501e335c3
LeonardoAndrad3 <leonardoxcontato@gmail.com>
2025-01-14 22:23:53 -0300

    adding new content to the main menu from employee

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import {ContainerMain, DivPerfil, DivPainel, IconPerfil} from "./styled"
+import { ButtonCustom } from "components/buttonBack/styled";
+import { H1 } from "styles/Global/styled";
+import "./styled.css";
 
 
 export default function App(){
     return(
-        <>
-            <h1>Welcome to charis!</h1>
+        <ContainerMain>
 
-            <Link to="/login/form">Login</Link>
+            <DivPerfil>
+                <div>
+                    <IconPerfil/>
+                    <p id="name">Mariana</p>
+                    <p id="work">Nail designer</p>
+                </div>
+ 
+    
+                <ButtonCustom>config</ButtonCustom>
         
-        </>
+            </DivPerfil>
+            <hr></hr>
+            <DivPainel>
+                <H1>Painel geral</H1>
+                
+                <ButtonCustom>Agenda<span className="agenda icon"></span></ButtonCustom>
+                <ButtonCustom>Calendário<i className="calendario icon"></i></ButtonCustom>
+                <ButtonCustom>Registro<i className="registro icon"></i></ButtonCustom>
+                <ButtonCustom>Personalizar<i className="personalizar icon"></i></ButtonCustom>
+                <ButtonCustom id="dash">DashBoard<i className="dashboard icon"></i></ButtonCustom>
+            </DivPainel>
+
+        
+        </ContainerMain>
     );
 }
